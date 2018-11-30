fa87297764962791665ca920ea1f594a5bc48e5f
ajinkyag911 <ajinkyag911@gmail.com>
2018-11-30 14:28:40 +0530

    Hello World and Hello Toast Added

---
+++ HelloToast/app/src/main/java/com/example/ajinkya/hellotoast/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.ajinkya.hellotoast;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mCountTextView;
+    private int mCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mCount = 0;
+        mCountTextView=findViewById(R.id.show_count);
+        mCountTextView.setText(Integer.toString(mCount));
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        mCountTextView.setText(Integer.toString(mCount));
+    }
+}
