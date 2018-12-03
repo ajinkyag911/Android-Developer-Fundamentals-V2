d260bd2530fdb8d80c553757d211f11b363c5b71
ajinkyag911 <ajinkyag911@gmail.com>
2018-12-03 18:34:00 +0530

    Implicit Intents Challenge Complete

--- CodingChallenges/LifecycleChallenge/app/src/main/java/com/example/ajinkya/lifecyclechallenge/MainActivity.java
+++ CodingChallenges/LifecycleChallenge/app/src/main/java/com/example/ajinkya/lifecyclechallenge/MainActivity.java
@@ -1,24 +1,29 @@
 package com.example.ajinkya.lifecyclechallenge;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int TEXT_REQUEST = 1;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
     String[] vegetables = {"Carrots", "Tomatoes", "Cucumbers", "Spinach", "Onions"};
     private TextView[] item;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         item = new TextView[5];
+        mLocationEditText = findViewById(R.id.loc_editText);
         item[0] = findViewById(R.id.item1);
         item[1] = findViewById(R.id.item2);
         item[2] = findViewById(R.id.item3);
@@ -45,5 +50,18 @@ public class MainActivity extends AppCompatActivity {
     private void setItem(int itemId) {
         Log.d("setItem", "Hello");
         item[itemId - 1].setText(vegetables[itemId - 1]);
+    }
+
+    public void openLocation(View view) {
+        String loc = mLocationEditText.getText().toString();
+
+        Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d(LOG_TAG, "Cannot open location!");
+        }
     }
 }
